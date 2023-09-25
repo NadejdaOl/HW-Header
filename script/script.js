0d9fa1387a727dc8afac9a9dceef10513fe1267b
Nadiia <nadusharikol@gmail.com>
2023-09-25 08:52:28 +0200

    new section

--- script/script.js
+++ script/script.js
@@ -37,10 +37,129 @@ for (let i = 0; i <= 3; i++) {
     let h5 = document.createElement('h5');
     h5.textContent = 'на рынке';
     box.appendChild(h5);
-
 }
+sectionFirst.appendChild(container);
 
-sectionFirst.appendChild(container)
+const sectionSecond = document.createElement('section');
+sectionSecond.classList.add('section-second');
+root.appendChild(sectionSecond);
+// document.body.appendChild(root);
+
+const h2 = document.createElement('h2');
+h2.textContent = 'НАШИ САМЫЕ БОЛЬШИЕ ПРОЕКТЫ';
+sectionSecond.appendChild(h2);
+
+const container2 = document.createElement('div');
+container2.classList.add('bigContainer');
+
+for (let i = 0; i <= 2; i++) {
+    let project = document.createElement('div');
+    project.classList.add('project-container');
+    container2.appendChild(project);
+
+    let img = document.createElement('img');
+    img.src = "URL('HW-Header/media/Project.svg')";
+    project.appendChild(img);
+
+    let hr= document.createElement('hr');
+    project.appendChild(hr);
+
+    let h4= document.createElement('h4');
+    h4.textContent = 'Арена';
+    project.appendChild(h4);
+
+    let firstItem = document.createElement('p');
+    firstItem.textContent = 'Мы сделали самую красивую арену в Европе. Это открытие стало для нас прорывной точкой для разивтия на следующие десятилетия. Мы очень рады данному еву.';
+    project.appendChild(firstItem);
+    container2.appendChild(project);
+}
+sectionSecond.appendChild(container2);
+
+const request = document.createElement('section');
+request.classList.add('sectionRequest');
+root.appendChild(request);
+// document.body.appendChild(root);
+
+const divItems = document.createElement('div');
+divItems.classList.add('div-items');
+request.appendChild(divItems);
+
+const divForItems = document.createElement('div');
+divForItems.classList.add('div-for-items');
+
+
+const nextSection = document.createElement('h2');
+nextSection.textContent = 'САМЫЕ УМНЫЕ ПРОЕКТЫ';
+divForItems.appendChild(nextSection);
+
+const littleSection = document.createElement('h3');
+littleSection.textContent = 'РЕАЛИЗУЕМ САМЫЕ СМЕЛЫЕ РЕШЕНИЯ В РОССИИ!';
+divForItems.appendChild(littleSection);
+
+divItems.appendChild(divForItems);
+
+
+const button = document.createElement('button');
+button.innerHTML = 'ВАШ ЗАПРОС';
+button.classList.add('customButton');
+divItems.appendChild(button);
+
+const services = document.createElement('section');
+services.classList.add('sectionServices');
+root.appendChild(services);
+
+const lightServices = document.createElement('h2');
+lightServices.textContent = 'ЧЕМ МЫ ЗАНИМАЕМСЯ?';
+services.appendChild(lightServices);
+
+const serviceItem = document.createElement('div');
+serviceItem.classList.add('service_item');
+
+for (let i = 0; i <= 2; i++) {
+    let group = document.createElement('div');
+    group.classList.add('group');
+    serviceItem.appendChild(group);
+
+    let img = document.createElement('img');
+    img.src = "URL('HW-Header/media/home.svg')";
+    group.appendChild(img);
+
+    let h4= document.createElement('h4');
+    h4.textContent = 'СТРОИТЕЛЬСТВО ОФИСНЫХ ЗДАНИЙ';
+    group.appendChild(h4);
+
+    // services.appendChild(serviceItem);
+}
+services.appendChild(serviceItem);
+
+const services1 = document.createElement('section');
+services1.classList.add('sectionServices1');
+root.appendChild(services1);
+
+const lightServices1 = document.createElement('h2');
+lightServices1.textContent = 'ЧЕМ МЫ ЗАНИМАЕМСЯ?';
+services1.appendChild(lightServices1);
+
+const serviceItem1 = document.createElement('div');
+serviceItem1.classList.add('service_item1');
+
+for (let i = 0; i <= 5; i++) {
+    let group1 = document.createElement('div');
+    group1.classList.add('group1');
+    serviceItem1.appendChild(group1);
+
+    let img = document.createElement('img');
+    img.src = "URL('HW-Header/media/home.svg')";
+    group1.appendChild(img);
+
+    let h4= document.createElement('h4');
+    h4.textContent = 'СТРОИТЕЛЬСТВО ОФИСНЫХ ЗДАНИЙ';
+    group1.appendChild(h4);
+}
+    services1.appendChild(serviceItem1);
+
+
+
 
 
 
